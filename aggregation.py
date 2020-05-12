7eb1b37b2d226c707b04d255a5f9eb7872add2d7
YuliaKorysheva <yuliakoryshevaa@gmail.com>
2020-05-12 15:34:36 +0300

    py files have changed

--- aggregation.py
+++ aggregation.py
@@ -1,23 +1,76 @@
 import pandas as pd
 from typing import Optional, List
 
-class AggregationData():
-    '''Data Aggregation Tools
+def create_empty_data(data: pd.DataFrame) -> pd.DataFrame:
+    '''Creates an empty file for further filling
+
+    Args:
+    data(pd.DataFrame) = data for creating indexes
     '''
-    def aggregate_category_entries(data: pd.DataFrame, feature_for_aggregation: List[str]) -> pd.DataFrame:
-        '''Aggregates characteristics by counting occurrences of individual categories
-        Creates tags for each unique value for all categories. For each client, counts the number of each category.
+    return pd.DataFrame({'SK_ID_CURR': data['SK_ID_CURR'].unique()}).set_index('SK_ID_CURR')
 
-        Args:
-        data(pd.DataFrame) = aggregation data
-        feature_for_aggregation(List[str]) = features to be aggregated
-        '''
-        column_name = 'SK_ID_CURR'
-        d = pd.DataFrame({column_name: data[column_name].unique()}).set_index(column_name)
-        for feature in feature_for_aggregation:
-            d = d.reindex(columns = d.columns.tolist() + list(data[feature].unique()))
-            group = data.groupby([column_name, feature]).count()
-            for idx, idx_feature in group.index:
-                d.loc[idx, idx_feature] = group['SK_ID_BUREAU'].loc[(idx, idx_feature)]
+def aggregate_category_entries(data: pd.DataFrame, feature_for_aggregation: List[str]) -> pd.DataFrame:
+    '''Aggregates characteristics by counting occurrences of individual categories
+    Creates tags for each unique value for all categories. For each client, counts the number of each category.
 
-        return d
+    Args:
+    data(pd.DataFrame) = aggregation data
+    feature_for_aggregation(List[str]) = features to be aggregated
+    '''
+    d = create_empty_data(data)
+    for feature in feature_for_aggregation:
+        d = d.reindex(columns = d.columns.tolist() + list(data[feature].unique()))
+        group = data.groupby(['SK_ID_CURR', feature]).count()
+        for idx, idx_feature in group.index:
+            d.loc[idx, idx_feature] = group['SK_ID_BUREAU'].loc[(idx, idx_feature)]
+
+    return d
+
+def aggregate_max_value(data: pd.DataFrame, feature_for_aggregation: List[str]) -> pd.DataFrame:
+    '''Creates new characteristics based on the maximum values ​​of each characteristic
+
+    Args:        
+    data(pd.DataFrame) = aggregation data
+    feature_for_aggregation(List[str]) = features to be aggregated
+    '''
+    d = create_empty_data(data)
+    for feature in feature_for_aggregation:
+        d = d.reindex(columns = d.columns.tolist() + [feature])
+        group = data.groupby(['SK_ID_CURR'])[feature].max()
+        for idx in group.index:
+            d.loc[idx, feature] = group.loc[idx]
+            
+    return d.rename(columns=lambda x: x + '_MAX')
+
+def aggregate_min_value(data: pd.DataFrame, feature_for_aggregation: List[str]) -> pd.DataFrame:
+    '''Creates new characteristics based on the minimum values ​​of each characteristic
+
+    Args:        
+    data(pd.DataFrame) = aggregation data
+    feature_for_aggregation(List[str]) = features to be aggregated
+    '''
+    d = create_empty_data(data)
+    for feature in feature_for_aggregation:
+        d = d.reindex(columns = d.columns.tolist() + [feature])
+        group = data.groupby(['SK_ID_CURR'])[feature].min()
+        for idx in group.index:
+            d.loc[idx, feature] = group.loc[idx]
+            
+    return d.rename(columns=lambda x: x + '_MIN')
+
+def aggregate_mean_value(data: pd.DataFrame, feature_for_aggregation: List[str]) -> pd.DataFrame:
+    '''Creates new characteristics based on the mean values ​​of each characteristic
+
+    Args:        
+    data(pd.DataFrame) = aggregation data
+    feature_for_aggregation(List[str]) = features to be aggregated
+    '''
+    d = create_empty_data(data)
+    for feature in feature_for_aggregation:
+        d = d.reindex(columns = d.columns.tolist() + [feature])
+        group = data.groupby(['SK_ID_CURR'])[feature].mean()
+        for idx in group.index:
+            d.loc[idx, feature] = group.loc[idx]
+            
+    return d.rename(columns=lambda x: x + '_MEAN')
+    
--- analytics.py
+++ analytics.py
@@ -1,56 +1,52 @@
 import pandas as pd
 from typing import Optional, List
 
-class Analysis:
-    '''Data analysis tool
+def check_missing_values(data: pd.DataFrame, number_of_lines: int, axis_check: int) -> pd.DataFrame:
+    '''Returns columns with the number of passes (descending)
+
+    Args:
+    data(pd.DataFrame) = data for checking
+    number_of_lines(int) = the number of lines to be printed
+    axis_check(int) = analyze rows(1) or columns(0)
     '''
+    return data.isnull().sum(axis=axis_check).sort_values(ascending=False)[:number_of_lines]
 
-    def check_missing_values(data: pd.DataFrame, number_of_lines: int, axis_check: int) -> pd.DataFrame:
-        '''Returns columns with the number of passes (descending)
+def get_unique_value(data: pd.DataFrame, features: Optional[List[str]]=None):
+    '''Returns unique features values
+
+    Args:
+    data(pd.DataFrame) =  data for checking
+    features(List[str]) = features that need to be checked for omissions
+        Defaul value: None
+    '''
+    if features is None:
+        features = list(data)
     
-        Args:
-        data(pd.DataFrame) = data for checking
-        number_of_lines(int) = the number of lines to be printed
-        axis_check(int) = analyze rows(1) or columns(0)
-        '''
-        return data.isnull().sum(axis=axis_check).sort_values(ascending=False)[:number_of_lines]
+    for feature in features:
+        print(feature, type(data[feature][0]), data[feature].unique())
 
-    def get_unique_value(data: pd.DataFrame, features: Optional[List[str]]=None):
-        '''Returns unique features values
+def get_features_with_omissions(data: pd.DataFrame) -> List[str]:
+    '''Returns a list of features that have omissions
+
+    Args:
+    data(pd.DataFrame) =  data for checking
+    '''
+    return [feature for feature in list(data) if data[feature].isnull().any()]
+
+def get_lists_type_feature(data: pd.DataFrame, features: List[str]) -> [List[str], List[str]]:
+    '''Separates features by type
     
-        Args:
-        data(pd.DataFrame) =  data for checking
-        features(List[str]) = features that need to be checked for omissions
-            Defaul value: None
-        '''
-        if features is None:
-            features = list(data)
-        
-        for feature in features:
-            print(feature, type(data[feature][0]), data[feature].unique())
-
-    def get_features_with_omissions(data: pd.DataFrame) -> List[str]:
-        '''Returns a list of features that have omissions
+    Args:
+    data(pd.DataFrame) = data for checking
+    features(List[str]) = features that need to be divided into string and numeric
+    '''
+    features_str = []
+    features_int = []
+    for feature in features:
+        if type(data[feature][0]) == str:
+            features_str.append(feature)
+        else:
+            features_int.append(feature)
     
-        Args:
-        data(pd.DataFrame) =  data for checking
-        '''
-        return [feature for feature in list(data) if data[feature].isnull().any()]
-
-    def get_lists_type_feature(data: pd.DataFrame, features: List[str]) -> [List[str], List[str]]:
-        '''Separates features by type
-        
-        Args:
-        data(pd.DataFrame) = data for checking
-        features(List[str]) = features that need to be divided into string and numeric
-        '''
-        features_str = []
-        features_int = []
-        for feature in features:
-            if type(data[feature][0]) == str:
-                features_str.append(feature)
-            else:
-                features_int.append(feature)
-        
-        return features_str, features_int
+    return features_str, features_int
         
